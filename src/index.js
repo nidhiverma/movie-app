822f395c8e798e0a32783aa47758d76225439473
nidhiverma1997 <nidhiverma1997@gmail.com>
2021-03-16 15:17:54 +0530

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 
-import "./index.css";
-import App from "./components/App";
-import rootReducer from "./reducers";
+import './index.css';
+import App from './components/App';
+import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
-console.log("store", store);
-console.log("Initital State", store.getState());
+// function logger(obj, next, action)
+// function logger(obj)(next)(action)
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // middleware code
+  console.log('ACTION TYPE = ', action.type);
+  next(action);
+};
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+console.log('store', store);
+console.log('Initital State', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
     <App store={store} />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
